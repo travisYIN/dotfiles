1a800c13c064a0084e68d642f4d680fb1b2d42ef
Edward Chu <crazyzyt@gmail.com>
2015-12-29 02:40:09 +0800

    update from repo

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,10 +1,8 @@
 ### Autojump
-
-[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh
+[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
 ### Oh My Zsh
-
 ZSH_THEME="agnoster"
-plugins=(git osx autojump ruby sublime brew extract)
+plugins=(git osx autojump ruby sublime brew history atom dirhistory)
 
 source $ZSH/oh-my-zsh.sh
