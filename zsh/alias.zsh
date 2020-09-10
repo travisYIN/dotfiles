2dc2d1dbf3396057ce91afd969e9221027acc404
qbyin <qbyin@tencent.com>
2020-09-10 11:32:37 +0800

    feat: my config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,13 +3,28 @@ alias cat="ccat"
 
 alias ofd="open ."
 alias zshrl="source ~/.zshrc"
-alias zshrc="st ~/.zshrc"
-alias tmrc="st ~/.tmux.conf"
-alias hosts="st /etc/hosts"
-alias quickp="git add . & git commit -m 'quick commit' & git push"
+alias zshrc="code ~/.zshrc"
+alias tmrc="code ~/.tmux.conf"
+alias hosts="code /etc/hosts"
+alias quickp="sudo git add . & sudo git commit -m 'feat: quick commit' & sudo git push origin $(git_current_branch)"
+alias s774="sudo chmod 774"
+
+# git aliases
+alias gs='git status'
+gbdp() { git branch -D $@ && git push origin :$@ }
+
+# bundle aliases
+alias bdpod="bundle exec pod"
 
 # tmux aliases
 alias ta='tmux attach'
 alias tls='tmux ls'
 alias tat='tmux attach -t'
 alias tns='tmux new-session -s'
+
+# hippy
+alias hippy='node ./scripts/local-cli/hippy.js run --project'
+alias adbr='adb reverse tcp:8082 tcp:8082 & adb reverse tcp:38989 tcp:38989'
+
+# wechattool
+alias cli="/Applications/wechatwebdevtools.app/Contents/MacOS/cli"
